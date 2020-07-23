9d4c3f734ac880410bb4402820a8bb461e5106a2
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-23 11:12:02 -0300

    test: add authentication tests

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -32,4 +32,47 @@ describe('CreateUser', () => {
 		expect(response).toHaveProperty('token');
 		expect(response.user).toEqual(user);
 	});
+
+	it('should not be able to authenticate with not existing user', async () => {
+		const fakeUsersRepository = new FakeUsersRepository();
+		const fakeHashProvider = new FakeHashProvider();
+
+		const authenticateUser = new AuthenticateUserService(
+			fakeUsersRepository,
+			fakeHashProvider,
+		);
+
+		expect(
+			authenticateUser.execute({
+				email: 'email@email.com',
+				password: 'password',
+			}),
+		).rejects.toBeInstanceOf(AppError);
+	});
+
+	it('should not be able to authenticate with wrong password', async () => {
+		const fakeUsersRepository = new FakeUsersRepository();
+		const fakeHashProvider = new FakeHashProvider();
+
+		const createUser = new CreateUserService(
+			fakeUsersRepository,
+			fakeHashProvider,
+		);
+		const authenticateUser = new AuthenticateUserService(
+			fakeUsersRepository,
+			fakeHashProvider,
+		);
+
+		await createUser.execute({
+			email: 'email@email.com',
+			password: 'password',
+		});
+
+		expect(
+			authenticateUser.execute({
+				email: 'email@email.com',
+				password: 'wrong-password',
+			}),
+		).rejects.toBeInstanceOf(AppError);
+	});
 });
